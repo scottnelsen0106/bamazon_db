9630dc803825c880f9c0cf24d069893c0a514612
Scott Nelsen <snelsen0106@gmail.com>
2019-10-16 22:42:16 -0500

    got database to update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,11 +18,29 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
 
-  displayProducts()
+  displayProducts();
  
 });
  
 function displayProducts() {
+
+inquirer.prompt({
+  name: "Buy",
+  type: "list",
+  message: "Would you like to select a product?",
+  choices: ["BUY", "EXIT"]
+
+})
+
+.then(function(response){
+
+  if(response.Buy === "BUY") {
+    buyProducts();
+  } else if (response.Buy ==="EXIT"){
+    connection.end();
+  }
+
+})
   var productsTable = "SELECT * FROM products";
 
   connection.query(productsTable, function(err, results){
@@ -32,3 +50,50 @@ function displayProducts() {
     
   })
 }
+
+function buyProducts() {
+  inquirer.prompt([
+  {
+    name: "ID",
+    type: "input",
+    message: "Please select an item by id number for purchase",
+    filter: Number
+    
+  },
+  {
+      name: "itemQty",
+      type: "input",
+      message: "How many items do you want",
+      filter: Number
+  }
+])
+
+ .then(function(answer) {
+   var unitsNeeded = answer.itemQty;
+   var idNeeded = answer.ID;
+  checkout(unitsNeeded,idNeeded);
+})
+}
+
+    
+ function checkout(ID ,totalNeeded) {
+  connection.query("SELECT * FROM products WHERE item_id = " + ID, function(err, res){
+    if(err){
+      console.log(err)
+    }
+
+    if(totalNeeded<=res[0].stock_quantity){
+      var cost = res[0].price * totalNeeded;
+      connection.query("UPDATE products SET stock_quantity = stock_quantity - " + totalNeeded + " WHERE item_id = " + ID);
+
+      console.log("Your total is " + cost);
+
+      
+    }
+    displayProducts();
+  })
+  
+ }
+
+ 
+
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -2,14 +2,15 @@ drop database if exists bamazon_db;
 create database bamazon_db;
 use bamazon_db;
 
-create table products (
-item_id  int auto_increment not null,
-product_name varchar(30) not null,
-department_name varchar(30) not null,
-price int not null,
-stock_quantity int,
-primary key(item_id)
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(50) NULL,
+  price DECIMAL(7,2) NOT NULL,
+  stock_quantity INT NOT NULL DEFAULT '1',
+  PRIMARY KEY (item_id)
 );
+Select * From products;
 
 insert into products(product_name, department_name, price, stock_quantity)
 values("tv", "electronics", 300, 6),
